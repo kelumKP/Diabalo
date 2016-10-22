6580115c98a6446ffd6d89dd2ec95ffe30de38a6
kelumKP <ke.pedson@compsoc.lk>
2016-10-23 00:00:57 +0530

    Authers changed as Authors , authers changed as authors

---
+++ LibraryApp.WebAPI/Controllers/AuthorsController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using LibraryApp.Core.Entities;
+using LibraryApp.Infrastructure;
+
+namespace LibraryApp.WebAPI.Controllers
+{
+    public class AuthorsController : ApiController
+    {
+        private LibraryRepository db = new LibraryRepository();
+
+        // GET: api/Authors
+        public IEnumerable<Author> GetAuthors()
+        {
+            return db.GetAuthors();
+        }
+
+        // GET: api/Authors/5
+        [ResponseType(typeof(Author))]
+        public IHttpActionResult GetAuthor(int id)
+        {
+            Author author = db.FindAuthorById(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(author);
+        }
+
+        // PUT: api/Authors/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutAuthor(int id, Author author)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != author.Auth_Id)
+            {
+                return BadRequest();
+            } 
+          
+            db.EditAuthor(author);               
+
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Authors
+        [ResponseType(typeof(Author))]
+        public IHttpActionResult PostAuthor(Author author)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.AddAuthor(author);
+
+            return CreatedAtRoute("DefaultApi", new { id = author.Auth_Id }, author);
+        }
+
+        // DELETE: api/Authors/5
+        [ResponseType(typeof(Author))]
+        public IHttpActionResult DeleteAuthor(int id)
+        {
+            Author author = db.FindAuthorById(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            db.RemoveAuthor(id);
+
+            return Ok(author);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                //db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        // GET: api/Authors
+
+        [System.Web.Http.Route("api/Authors/all")] 
+        public IEnumerable<Basic> GetAuthorsIdName()
+        {
+            return db.GetAuthorsIdName();
+        }
+
+        // GET: api/Authors
+
+        [System.Web.Http.Route("api/Authors/EmploymentStatus")]
+        public IEnumerable<Basic> GetEmplyeeStatusIdNameWebAPI()
+        {
+            return db.GetEmplyeeStatusIdName();
+        }
+
+    }
+}
---
+++ LibraryApp.WebAPI/Controllers/BooksWithAuthorsController.cs
@@ -0,0 +1,62 @@
+﻿using LibraryApp.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using LibraryApp.Core.Entities;
+using System.Web.Http.Description;
+
+namespace LibraryApp.WebAPI.Controllers
+{
+    public class BooksWithAuthorsController : ApiController
+    {
+       
+        private LibraryRepository db = new LibraryRepository();
+
+        // GET: api/BooksWithAuthors
+        public IEnumerable<BookWithAuthor> GetBookswithAuthors()
+        {
+            return db.GetBooksWithAuthors();
+        }
+
+        // GET: api/BooksWithAuthors/3
+
+        [ResponseType(typeof(BookWithAuthor))]
+        public IHttpActionResult GetBooksWithAuthorsById(int id)
+        {
+            BookWithAuthor bookwithauthor = db.FindBooksWithAuthorsById(id);
+            if (bookwithauthor == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(bookwithauthor);
+        }
+
+        // GET: api/BooksWithAuthors/3/Price  
+
+        [ResponseType(typeof(decimal))]
+        [System.Web.Http.Route("api/BooksWithAuthors/{id}/Price")]
+        public IHttpActionResult GetBooksPriceById(int? id)
+        {
+            decimal bookprice = db.findBookPrice(id);
+
+            return Ok(bookprice);
+        }
+
+        // GET: api/BooksWithAuthors/3/booktitle/Price 
+
+        [ResponseType(typeof(decimal))]
+        [System.Web.Http.Route("api/BooksWithAuthors/{id}/{name}/Price")]
+        public IHttpActionResult GetBooksPriceById(int? id, string name = null)
+        {
+            decimal bookprice = db.findBookPrice(id,name);
+
+            return Ok(bookprice);
+        }
+
+
+    }
+}
